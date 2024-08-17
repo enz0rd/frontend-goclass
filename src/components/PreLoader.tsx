27f0a5ecb6007aca1b5ac49b171800dfa539dbe8
Enzo <enzorossidaltoe@hotmail.com.br>
2024-08-16 22:03:58 -0300

    init

--- src/components/PreLoader.tsx
+++ src/components/PreLoader.tsx
@@ -1,11 +1,10 @@
 import React, { useEffect, useState } from "react";
-import { useLocation, useNavigate, useNavigationType } from "react-router-dom";
+import { useLocation, useNavigationType } from "react-router-dom";
 import './css/preloader.css'
 
 const PreLoader: React.FC = () => {
     const [loading, setLoading] = useState(true);
     const location = useLocation();
-    const navigate = useNavigate();
     const navigationType = useNavigationType();
 
     useEffect(() => {
