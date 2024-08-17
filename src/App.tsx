757168b9679ad420cfa5236cf1a63d6933a6ffad
Enzo <enzorossidaltoe@hotmail.com.br>
2024-08-16 22:02:25 -0300

    init

--- src/App.tsx
+++ src/App.tsx
@@ -11,7 +11,7 @@ function App() {
 
   return (
       <Router>
-        <PreLoader />
+        <PreLoader/>
         <Routes>
             <Route path='/' element={<HomePage/>} />
             <Route path='/login' element={<LoginPage/>} />
