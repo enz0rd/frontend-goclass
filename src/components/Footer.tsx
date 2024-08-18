ded41dc15c72f5489d6499700e3cb92a63ef6426
Enzo <enzorossidaltoe@hotmail.com.br>
2024-08-18 13:56:44 -0300

    added logo

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,4 +1,4 @@
-import darkLogo from "/assets/Logos/GoclassLogo-light.svg";
+import darkLogo from "@/assets/Logos/GoclassLogo-light.svg";
 import { NavLink } from "react-router-dom";
 import { FaInstagram } from "react-icons/fa6";
 import { SiLinktree } from "react-icons/si";
