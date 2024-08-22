378a0d7d5409bf12418cf15d97a1d796a3644ed1
Enzo Rossi Daltoé <enzorossidaltoe@hotmail.com.br>
2024-08-22 11:32:19 -0300

    added lazy loading

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,15 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, Suspense, lazy } from "react";
 import { Route, Routes, useLocation } from "react-router-dom";
-import HomePage from "@/pages/HomePage";
-import LoginPage from "@/pages/LoginPage";
-import RegisterPage from "@/pages/RegisterPage";
 import { AnimatePresence, motion } from "framer-motion";
 import Preloader from "@/components/PreLoader";
-import SuccessPage from "@/pages/SuccessPage";
-import CancelPage from "@/pages/CancelPage";
-import DashboardPage from "./pages/DashboardPage";
+
+// Importação dinâmica dos componentes
+const HomePage = lazy(() => import("@/pages/HomePage"));
+const LoginPage = lazy(() => import("@/pages/LoginPage"));
+const RegisterPage = lazy(() => import("@/pages/RegisterPage"));
+const SuccessPage = lazy(() => import("@/pages/SuccessPage"));
+const CancelPage = lazy(() => import("@/pages/CancelPage"));
+const DashboardPage = lazy(() => import("./pages/DashboardPage"));
 
 function App() {
   const location = useLocation();
@@ -23,25 +25,27 @@ function App() {
       {loading ? (
         <Preloader key="preloader" />
       ) : (
-        <motion.div
-          style={{
-            width: "100%",
-            height: "100%",
-          }}
-          initial={{ opacity: 0 }}
-          animate={{ opacity: 1 }}
-          exit={{ opacity: 0 }}
-          transition={{ duration: 0.7 }}
-        >
-          <Routes location={location} key={location.pathname}>
-            <Route path="/" element={<HomePage />} />
-            <Route path="/login" element={<LoginPage />} />
-            <Route path="/register" element={<RegisterPage />} />
-            <Route path="/register/success" element={<SuccessPage />} />
-            <Route path="/register/cancel" element={<CancelPage />} />
-            <Route path='/dashboard' element={<DashboardPage />} />
-          </Routes>
-        </motion.div>
+        <Suspense fallback={<div>Loading...</div>}>
+          <motion.div
+            style={{
+              width: "100%",
+              height: "100%",
+            }}
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            exit={{ opacity: 0 }}
+            transition={{ duration: 0.7 }}
+          >
+            <Routes location={location} key={location.pathname}>
+              <Route path="/" element={<HomePage />} />
+              <Route path="/login" element={<LoginPage />} />
+              <Route path="/register" element={<RegisterPage />} />
+              <Route path="/register/success" element={<SuccessPage />} />
+              <Route path="/register/cancel" element={<CancelPage />} />
+              <Route path="/dashboard" element={<DashboardPage />} />
+            </Routes>
+          </motion.div>
+        </Suspense>
       )}
     </AnimatePresence>
   );
