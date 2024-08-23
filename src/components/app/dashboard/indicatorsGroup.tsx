0970a8f3180a60ad49a833129f301dbb7ca80ae2
Enzo Rossi Daltoé <enzorossidaltoe@hotmail.com.br>
2024-08-23 11:16:07 -0300

    refined dashboard

--- src/components/app/dashboard/indicatorsGroup.tsx
+++ src/components/app/dashboard/indicatorsGroup.tsx
@@ -2,11 +2,11 @@ import MainIndicator from "./Indicators/MainIndicator";
 
 const IndicatorsGroup = () => {
     return (
-        <div className="grid grid-cols-1 w-full mx-auto md:grid-cols-2 lg:grid-cols-4 gap-2 place-items-center">
-            <MainIndicator title="Atividades Pendentes" number={5} colorStyle={{"light": "via-red-300/40", "dark": "dark:via-red-700/40"}} />
-            <MainIndicator title="Novas atividades" number={2} colorStyle={{"light": "via-yellow-300/40", "dark": "dark:via-yellow-700/40"}} />
-            <MainIndicator title="Avaliações" number={1} colorStyle={{"light": "via-green-300/40", "dark": "dark:via-green-700/40"}} />
-            <MainIndicator title="Mensagens" number={10} colorStyle={{"light": "via-primary/40", "dark": "dark:via-primary/40"}} />
+        <div className="flex space-x-4">
+            <MainIndicator title="Atividades Pendentes" number={5} colorStyle={{ "light": "via-red-300/40", "dark": "dark:via-red-700/40" }} />
+            <MainIndicator title="Novas atividades" number={2} colorStyle={{ "light": "via-yellow-300/40", "dark": "dark:via-yellow-700/40" }} />
+            <MainIndicator title="Avaliações" number={1} colorStyle={{ "light": "via-green-300/40", "dark": "dark:via-green-700/40" }} />
+            <MainIndicator title="Mensagens" number={10} colorStyle={{ "light": "via-primary/40", "dark": "dark:via-primary/40" }} />
         </div>
     )
 }
--- src/pages/DashboardPage.tsx
+++ src/pages/DashboardPage.tsx
@@ -1,7 +1,12 @@
 import IndicatorsGroup from "@/components/app/dashboard/indicatorsGroup";
 import NavbarApp from "@/components/app/NavbarApp";
+import { ScrollArea, ScrollBar } from "@/components/ui/scroll-area";
+import darkLogo from "@/assets/Logos/GoclassLogo-hat-light.png";
+import lightLogo from "@/assets/Logos/GoclassLogo-hat-dark.png";
+import { useTheme } from "@/ThemeProvider";
 
 const DashboardPage = () => {
+    const { isDarkMode } = useTheme(); // Access the theme context
     const auth = localStorage.getItem('authToken')
     if (!auth) window.location.href = '/login'
     const data = {
@@ -15,17 +20,19 @@ const DashboardPage = () => {
                 <div className="w-full">
                     <NavbarApp data={data} />
                     <img className="h-[25vh] w-full object-none" src="/images/success-classroom.jpg" alt="" />
-                    <div className="h-fit space-y-6 lg:px-[15rem] md:px-[6rem] px-4 bg-gradient-to-b dark:from-zinc-800 dark:to-zinc-900 from-zinc-100 to-zinc-200">
+                    <div className="h-screen space-y-6 lg:px-[15rem] md:px-[6rem] px-4 bg-gradient-to-b dark:from-zinc-800 dark:to-zinc-900 from-zinc-200 to-40% to-zinc-50">
                         <div className="pt-6 group dark:text-zinc-50 mx-4 flex justify-between align-middle">
-                            <h1 className="text-2xl lg:text-4xl md:text-4xl w-[40%]">Bem vindo, {data.user_name}</h1>
-                            <h1 className="text-sm lg:text-md md:text-md my-auto">O que faremos hoje?</h1>
-                        </div>
-                        <div className="w-[100%] flex md:flex-row lg:flex-col gap-4 group dark:text-zinc-50">
-                            <IndicatorsGroup />
-                            <div className="hidden md:flex lg:flex flex-col">
-                                <h1 className="text-2xl">Indicadores</h1>
-                                <span className="text-sm">Utilize esses indicadores</span>
+                            <div className="w-[40%]">
+                                <h1 className="text-2xl lg:text-4xl md:text-4xl">Bem vindo, {data.user_name}</h1>
+                                <span className="text-sm lg:text-md md:text-md my-auto">O que faremos hoje?</span>
                             </div>
+                            <img src={isDarkMode ? darkLogo : lightLogo} alt="Logo Goclass" className="w-[10%] object-contain" />
+                        </div>
+                        <div className="w-[100%] flex justify-center mx-auto align-middle">
+                            <ScrollArea className="p-4 overflow-x-auto whitespace-nowrap group dark:text-zinc-50">
+                                <IndicatorsGroup />
+                                <ScrollBar orientation="horizontal" />
+                            </ScrollArea>
                         </div>
                     </div>
                 </div>
